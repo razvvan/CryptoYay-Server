a830fc0788f3d08b7ee9d65110241a2903845d9e
razvvan <razvan@itnovate.ro>
2021-11-29 21:28:29 -0800

    pull from json

--- internal/serve/sendyay.go
+++ internal/serve/sendyay.go
@@ -1,15 +1,29 @@
 package serve
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/razvvan/CryptoYay-Server/internal/yay"
 )
 
+type SendYayRequest struct {
+	To  string `json:"to"`
+	Org string `json:"org"`
+}
+
 func SendYayHandler(w http.ResponseWriter, r *http.Request) {
-	err := yay.Send("to", "org")
+	syr := SendYayRequest{}
+
+	err := json.NewDecoder(r.Body).Decode(&syr)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("{\"status\": \"failed\"}"))
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = yay.Send(syr.To, syr.Org)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 }
